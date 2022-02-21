9a10bad4d430b5b05b6c084be845c2e814aee3a6
kiralyg12d <95190713+kiralyg12d@users.noreply.github.com>
2022-02-21 13:44:36 +0100

    8.feladat a,b,c

--- Nobel_2022_02_21/Form1.cs
+++ Nobel_2022_02_21/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -24,7 +25,31 @@ namespace Nobel_2022_02_21
 
         private void btnAdatolMentése_Click(object sender, EventArgs e)
         {
-
+            if (textBox1.Text.Length <= 1 || textBox2.Text.Length <= 1 || textBox3.Text.Length <= 1 || textBox4.Text.Length <= 1)
+            {
+                MessageBox.Show($"Töltsön ki minden mezőt!");
+               
+            }
+            if (int.Parse(textBox1.Text) <= 1989)
+            {
+                MessageBox.Show($"AZ évszám nem hibás!");
+                
+            }
+            try
+            {
+                StreamWriter sw = new StreamWriter("orvosi_nobeldijak.txt");
+                sw.WriteLine("Év;Név;Sz/H;Ország");
+                sw.WriteLine($"{textBox1.Text};{textBox2.Text};{textBox3.Text};{textBox4.Text}");
+                sw.Close();
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+            }
+            catch (Exception error )
+            {
+                MessageBox.Show($"Hiba a mentés során:\n{error.Message}");
+            }
         }
     }
 }
