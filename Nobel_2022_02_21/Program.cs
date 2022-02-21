84c6c02ac63031bc950dc9f5155423265133545f
kiralyg12d <95190713+kiralyg12d@users.noreply.github.com>
2022-02-21 10:32:08 +0100

    8.feladat

--- Nobel_2022_02_21/Program.cs
+++ Nobel_2022_02_21/Program.cs
@@ -13,7 +13,7 @@ namespace Nobel_2022_02_21
         /// </summary>
         [STAThread]
         static void Main()
-        {
+   {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
