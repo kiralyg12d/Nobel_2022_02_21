e01b2b20b510ddc54e5e2cb206bc0a0a9d74bbf7
kiralyg12d <95190713+kiralyg12d@users.noreply.github.com>
2022-02-21 10:30:41 +0100

    Add project files.

---
+++ Nobel_2022_02_21/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nobel_2022_02_21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdatolMentése_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
